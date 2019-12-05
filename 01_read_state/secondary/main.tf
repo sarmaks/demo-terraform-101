74e90b855cfd1eb88685e3ecf95237444d6f9b72
Seetarama Sarma <sarmaks@amazon.com>
2019-12-05 10:47:17 -0800

    changed the secondary/main.tf output to use 0.11.14 syntax

--- 01_read_state/secondary/main.tf
+++ 01_read_state/secondary/main.tf
@@ -8,5 +8,5 @@ data "terraform_remote_state" "primary" {
 }
 
 output "primary_public_ip" {
-  value = data.terraform_remote_state.primary.outputs.public_ip
+  value = "${data.terraform_remote_state.primary.public_ip}"
 }
